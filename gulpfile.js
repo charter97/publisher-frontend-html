e3a65f2bf66dd572e0a649bb051f2acc35008f06
Andrejs Vasiļevskis <andrew@appcloud.lv>
2020-11-15 22:20:18 +0200

    Resetting project

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@
 
 const gulp = require('gulp');
 const del = require('del');
-const autoprefixer = require('gulp-autoprefixer');
 const file_include = require('gulp-file-include');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const imagemin_png = require('imagemin-pngquant');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglifyjs');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('clear', function () {
+    return del('dist');
+});
 
 gulp.task('html', function () {
     return gulp.src('app/html/*.html')
@@ -22,9 +25,8 @@ gulp.task('sass', function () {
     return gulp.src('app/sass/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-        .pipe(sourcemaps.write())
         .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/css'));
 });
 
@@ -35,9 +37,7 @@ gulp.task('fonts', function () {
 
 gulp.task('scripts', function () {
     return gulp.src([
-        'app/libs/jquery/dist/jquery.js',
-        'app/libs/slick-carousel/slick/slick.min.js',
-        'app/libs/bootstrap/dist/js/bootstrap.js',
+        'node_modules/jquery/dist/jquery.slim.min.js',
         'app/js/common.js'
     ])
         .pipe(concat('main.min.js'))
@@ -45,11 +45,7 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('clean', function () {
-    return del('dist');
-});
-
-gulp.task('img', function () {
+gulp.task('images', function () {
     return gulp.src('app/img/**/*')
         .pipe(imagemin({
             interlaced: true,
@@ -60,17 +56,19 @@ gulp.task('img', function () {
         .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('watch', gulp.series('html', 'sass', function () {
-        gulp.watch('app/html/**/*.html', gulp.series('html'));
-        gulp.watch('app/sass/**/*.scss', gulp.series('sass'));
-    })
-);
-
-gulp.task('build', gulp.series(
-    'clean',
+gulp.task('build', gulp.series('clear', [
     'html',
     'sass',
     'fonts',
-    'img',
-    'scripts'
-));
+    'scripts',
+    'images'
+]));
+
+gulp.task('watch', gulp.series('build', function () {
+        gulp.watch('app/html/**/*.html', gulp.series('html'));
+        gulp.watch('app/sass/**/*.scss', gulp.series('sass'));
+        gulp.watch('app/fonts/*', gulp.series('fonts'));
+        gulp.watch('app/js/**/*.js', gulp.series('scripts'));
+        gulp.watch('app/img/**/*', gulp.series('images'));
+    })
+);

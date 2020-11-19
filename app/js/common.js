30ca62c6fd29b57270224a19c6306035a605b555
Andrejs Vasiļevskis <andrew@appcloud.lv>
2020-11-19 21:12:32 +0200

    Scroll to top button

--- app/js/common.js
+++ app/js/common.js
@@ -35,4 +35,18 @@
             }
         ]
     });
+
+    $(window).scroll(function () {
+        let top = $(window).scrollTop();
+        if (top > 100) {
+            $('#btn-scroll').show(300);
+        } else {
+            $('#btn-scroll').hide(300);
+        }
+    });
+
+    $('#btn-scroll').click(function () {
+        $("html, body").animate({scrollTop: 0}, 600);
+        return false;
+    });
 })();

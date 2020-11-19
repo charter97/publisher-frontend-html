984f514baaedeea0cdebd5ab76c698c160513616
Andrejs Vasiļevskis <andrew@appcloud.lv>
2020-11-19 18:22:00 +0200

    Header

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,11 @@
 (function () {
     'use strict';
 
+    $("#btn-category").click(function () {
+        $(this)
+            .toggleClass('icon-down-open icon-up-open')
+            .parent()
+            .find('ul')
+            .toggleClass('category-open');
+    });
 })();
